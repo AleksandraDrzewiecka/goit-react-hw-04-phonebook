a35e184770aa16c13690a364fc8265f002d6c491
AleksandraDrzewiecka <111142458+AleksandraDrzewiecka@users.noreply.github.com>
2023-04-20 19:36:51 +0200

    hooki use

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,77 +1,52 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import Filter from './Filter/Filter';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import css from './App.module.css';
-export class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      contacts: [],
-      filter: '',
-    };
-  }
-  componentDidMount() {
-    this.setState({ contacts: this.getLocalContacts() });
-  }
-  getLocalContacts = () => {
-    const localContacts = JSON.parse(localStorage.getItem('contacts') || '[]');
-    return localContacts;
+import {
+  getLocalContacts,
+  addLocalContact,
+  deleteLocalContact,
+} from './utils/AppFunc';
+
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    setContacts(getLocalContacts());
+  }, []);
+
+  const handleAddContact = contactToAdd => {
+    setContacts(oldContacts => [...oldContacts, contactToAdd]);
+    addLocalContact(contactToAdd);
   };
 
-  filterValueChange = evt => {
-    this.setState({ filter: evt.currentTarget.value.trim() });
+  const handleDeleteContact = contactToDelete => {
+    let newContacts = contacts.filter(
+      contact => contact !== contactToDelete
+    );
+    setContacts(newContacts);
+    deleteLocalContact(contactToDelete);
   };
 
-  filteredContacts = () => {
-    const { contacts, filter } = this.state;
-    const keyword = filter.toLowerCase();
-    const filteredContacts = contacts.filter(
-      contact => contact.name.toLowerCase().indexOf(keyword) > -1
-    );
-    return filteredContacts;
-  };
-  addContact = contact => {
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, contact],
-    }));
-    this.addLocalContact(contact);
-  };
-  addLocalContact = contact => {
-    const localContacts = this.getLocalContacts();
-    localContacts.push(contact);
-    localStorage.setItem('contacts', JSON.stringify(localContacts));
-  };
-  deleteContact = contactToDelete => {
-    const { contacts } = this.state;
-    let newContacts = contacts.filter(contact => contact !== contactToDelete);
-    this.setState({ contacts: newContacts });
-    this.deleteLocalContact(contactToDelete);
-  };
-  deleteLocalContact = contact => {
-    const localContacts = this.getLocalContacts();
-    const index = localContacts.findIndex(localContact => {
-      return localContact.id === contact.id;
-    });
-    localContacts.splice(index, 1);
-    localStorage.setItem('contacts', JSON.stringify(localContacts));
-  };
-  render() {
-    const { filter, contacts } = this.state;
-    return (
-      <section className={css.phonebook}>
-        <h1 className={css.phonebookTitle}>Phonebook</h1>
-        <ContactForm
-          contacts={contacts}
-          addContact={this.addContact}
-        ></ContactForm>
-        <h2 className={css.contactsTitle}>Contacts</h2>
-        <Filter filterValue={filter} onChange={this.filterValueChange}></Filter>
-        <ContactList
-          deleteContact={this.deleteContact}
-          filteredContacts={this.filteredContacts}
-        ></ContactList>
-      </section>
-    );
-  }
-}
+  return (
+    <section className={css.phonebook}>
+      <h1 className={css.phonebookTitle}>Phonebook</h1>
+      <ContactForm
+        contacts={contacts}
+        addContact={handleAddContact}
+      />
+      <h2 className={css.contactsTitle}>Contacts</h2>
+      <Filter
+        filterValue={filter}
+        onChange={evt => setFilter(evt.currentTarget.value.trim())}
+      />
+      <ContactList
+        deleteContact={handleDeleteContact}
+        contacts={contacts}
+        filter={filter}
+      />
+    </section>
+  );
+};
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,22 +1,32 @@
-import Contact from '../Contact/Contact';
+import React from 'react';
 import PropTypes from 'prop-types';
+import Contact from '../Contact/Contact';
 import css from './ContactList.module.css';
-const ContactList = ({ filteredContacts, deleteContact }) => (
-  <ul className={css.contactList}>
-    {filteredContacts().map(contact => (
-      <Contact
-        key={contact.id}
-        id={contact.id}
-        name={contact.name}
-        number={contact.number}
-        contact={contact}
-        deleteContact={deleteContact}
-      ></Contact>
-    ))}
-  </ul>
-);
+import { filteredContacts } from 'components/utils/ContactListFunc';
+
+function ContactList({ contacts, filter, deleteContact }) {
+  const filtered = filteredContacts(contacts, filter);
+
+  return (
+    <ul className={css.contactList}>
+      {filtered.map((contact) => (
+        <Contact
+          key={contact.id}
+          id={contact.id}
+          name={contact.name}
+          number={contact.number}
+          contact={contact}
+          deleteContact={deleteContact}
+        />
+      ))}
+    </ul>
+  );
+}
+
 ContactList.propTypes = {
-  filteredContacts: PropTypes.func.isRequired,
+  contacts: PropTypes.arrayOf(PropTypes.object),
   deleteContact: PropTypes.func.isRequired,
+  filter: PropTypes.string,
 };
+
 export default ContactList;
